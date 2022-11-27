a86fee7d43539ca32e4abbb53747abbd672a9058
kaloyanpeev <kaloyan.peev@mentormate.com>
2022-11-28 00:57:31 +0200

    Added utils folder with interface & axios updates

---
+++ src/utils/axios.ts
@@ -0,0 +1,5 @@
+export {};
+
+export const RapidApiHost: string = "wordsapiv1.p.rapidapi.com";
+export const RapidApiKey: string =
+  "ae0ee65316msh5d52e77399d0a49p179ca1jsn77e40476657d";
---
+++ src/utils/interfaces.ts
@@ -0,0 +1,7 @@
+export interface WordDataTypes {
+  word?: string;
+  results?: Object[];
+  syllables?: { count: string; list: string[] };
+  pronunciation?: Object;
+  frequency?: Object;
+}
