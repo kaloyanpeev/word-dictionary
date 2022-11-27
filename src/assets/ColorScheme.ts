e9ec409ee63eb04d0aacb2342ab552f760abb78c
kaloyanpeev <kaloyan.peev@mentormate.com>
2022-11-28 00:57:06 +0200

    Added color scheme

---
+++ src/assets/ColorScheme.ts
@@ -0,0 +1,14 @@
+const colors = {
+  color: "#6e7b98",
+  bg: "#2b2d3c",
+  bg2: "#13141c",
+  bg3: "#313448",
+  bg4: "#4d3840",
+  bg5: "#313448",
+  color2: "#d0d4e8",
+  color3: " #905f70",
+  color4: "#c0c3d6",
+  color5: "#c0c3d6",
+};
+
+export default colors;
